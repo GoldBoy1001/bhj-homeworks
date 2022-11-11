2bf55c666bd1cba00f28c0950d87a18e0e6c5a13
Sergio chaplin <Sergiorabota133@gmail.com>
2022-11-11 20:38:06 +0300

    homeworks client-state

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,14 @@
+const clearButton = document.querySelector('.clear_button');
+const editor = document.getElementById('editor');
+let textLocalStorage = localStorage.getItem('editorText');
+
+editor.value = textLocalStorage;
+editor.addEventListener('input', function () {
+   localStorage.setItem('editorText', editor.value);
+});
+
+clearButton.addEventListener('click', function () {
+   editor.value = '';
+   localStorage.removeItem('editorText');
+});
+
